c970f0bd7615366091faf5b43a2ea3b7798f2982
Thiago <tlcrezende@gmail.com>
2022-12-13 09:46:49 -0300

    Adicionado Login inicial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Footer from "./Footer";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
     <div>
-      TESTE<a>aff</a>
-      <Footer/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
+
 export default App;
