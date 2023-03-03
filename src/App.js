e82925dccd1565b68356f8a6e96e0d5b40547a3f
Thiago <tlcrezende@gmail.com>
2023-03-03 17:00:00 -0300

    add header and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
+//BrowserRouter deve englobar todo o App
+//Header e Footer estão fora de Routes porque eles são fixos para todo página, não muda.
 function App() {
   return (
     <div>
