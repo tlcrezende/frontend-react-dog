bf09630481ba4a732e1b7af1423d076105e1cf59
Thiago <tlcrezende@gmail.com>
2023-03-04 18:50:00 -0300

    add states data, loading, error and fetching

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { TOKEN_POST, USER_GET } from "./api";
 
 export const UserContext = React.createContext();
 
 export const UserStorage = ({ children }) => {
+  const [data, setData] = React.useState(null);
+  const [login, setLogin] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log("🚀 ~ file: UserContext.js:16 ~ getUser ~ json:", json)
+    
+    setData(json);
+    setLogin(true);
+  }
+
+  async function userLogin(username, password) {
+    const { url, options } = TOKEN_POST({ username, password });
+    const response = await fetch(url, options);
+    const json = await response.json();
+    window.localStorage.setItem("token", json.token);
+    getUser(json.token);
+  }
   return (
-    <UserContext.Provider value={{ usuario: "André" }}>
+    <UserContext.Provider value={{ userLogin, data }}>
       {children}
     </UserContext.Provider>
   );
