d69efbb0cc7758ced8da7d4d7cd5bc084469b08f
Thiago <tlcrezende@gmail.com>
2023-03-04 18:49:30 -0300

    add username in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
-import { ReactComponent as Dogs } from '../Assets/dogs.svg';
-
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { UserContext } from "../UserContext";
 
 const Header = () => {
   // <nav> é um container de navegação, geralmente usado para colocar os links de um menu
   // <Dogs/> é um svg da pasta assets, mas foi importado como um ReactComponente por causa do tipo de importação no começo do arquivo.
   // aria-label dentro do <Link> dá um título pequeno para a imagem
-
+  const { data } = React.useContext(UserContext);
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
