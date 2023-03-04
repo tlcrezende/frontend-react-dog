23db9b3c5c4dcb0c4a3ef56decc1041ef3db19e8
Thiago <tlcrezende@gmail.com>
2023-03-04 18:50:16 -0300

    api get user ready

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "../Form/Button";
+import Input from "../Form/Input";
+import useForm from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
+  const { userLogin } = React.useContext(UserContext);
 
-  function handleSubmit(event) {
-    // sempre lembrar de prevenir o default antes de fazer um fetch
+  async function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({username, password}),
-    }).then((response) => {
-        console.log(response);
-        return response.json();
-    }).then((json) => {
-        console.log(json);
-    })
+
+    // sempre lembrar de prevenir o default antes de fazer um fetch
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
   }
 
   return (
@@ -27,17 +24,19 @@ const LoginForm = () => {
       Login Form
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
+        <Input
+          label={"Usuário"}
+          type={"text"}
+          name={"username"}
+          {...username}
         />
-        <input
-          type="text"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
+        <Input
+          label={"Senha"}
+          type={"password"}
+          name={"password"}
+          {...password}
         />
-        <button>Entrar</button>
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </div>
